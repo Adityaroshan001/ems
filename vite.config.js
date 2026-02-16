a610a2ce7834f54ee6b6eac018e112ded8471459
Abhishek-mehata <mehtaabishekkumar99gmail.com>
2026-02-16 16:02:30 +0545

    deploy stage

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:"/ems"
   // other configurations can go here below
 })
